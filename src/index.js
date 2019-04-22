4b34184b63ce47e25c942dcd751a886a97a734f9
laravelProgrammer <laravelprogrammers@gmail.com>
2019-04-22 05:14:46 -0700

    Fifth Lecture 5

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+    <div>
+	<h1>Heloo Bitfumes</h1> 
+	<h1>Heloo Bitfumes</h1>
+    </div>,
+	document.getElementById('root')
+	);
